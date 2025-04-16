689bb7431b8f62b84dc22e503711d061e311b517
Annas Sovianto <141797683+Reigentsu@users.noreply.github.com>
2025-04-16 22:57:45 +0700

    tugas paas

--- client/scripts.js
+++ client/scripts.js
@@ -1,4 +1,4 @@
-const API_URL = "https://server-annas-298647753913.us-central1.run.app";
+const API_URL = "https://annas-server-298647753913.us-central1.run.app";
 let notes = [];
 let isEditing = false;
 
