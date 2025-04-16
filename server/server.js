063f2e37475c1fdd4a5f4f81dc475f6e1e885204
Annas Sovianto <141797683+Reigentsu@users.noreply.github.com>
2025-04-16 23:16:21 +0700

    update cors

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ import UserRoute from "./routes/noteRoutes.js";
 
 const app = express();
 app.use(
-  cors({ origin: "https://annas-client-dot-f-02-450706.uc.r.appspot.com" })
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 app.use(express.json());
 app.use(UserRoute);
